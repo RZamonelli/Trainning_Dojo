091fd6940ea8d5c86a23da61e9926f4933ffb716
Renan Zamonelli <47351828+RZamonelli@users.noreply.github.com>
2020-03-12 17:23:25 -0300

    terterter
    fgerer

---
+++ INSS/INSSUnit/Calculos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using INSS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace INSSUnit
+{
+    [TestClass]
+    public class Calculos
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var test = new Class1().Teste();
+
+            Assert.AreEqual("", test);
+        }
+    }
+}
---
+++ INSS/INSSUnit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("INSSUnit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("INSSUnit")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3ea7282b-3c08-4e4e-958f-3e2b1efb6def")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
