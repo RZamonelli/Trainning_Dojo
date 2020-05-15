e7bf9694f1e2238d52c7ade682eb678368296d06
Thiago Gaidzinski <thiago.gaidzinski@philips.com>
2020-05-15 15:45:36 -0300

    #7

--- INSS/INSS/CalculoIRRF.cs
+++ INSS/INSS/CalculoIRRF.cs
@@ -11,61 +11,26 @@ namespace INSS
     {
         public decimal CalcularIRRF(decimal renda, int dependentes)
         {
-            //var salario = new Salario();
-            //var calculo_IMP = salario.CalcularDescontoDependentes(renda, dependentes);
-            //decimal resultado = 0;
-
-            //if (calculo_IMP <= 1903.98m)
-            //{
-            //    resultado = 0;
-            //}
-            //else
-            //if (calculo_IMP >= 1903.08m && calculo_IMP <= 2826.65m)
-            //{
-            //    resultado = calculo_IMP * ConstantIRRF.IRRF0075;
-            //}
-            //else
-            //if (calculo_IMP >= 2826.65m && calculo_IMP <= 3751.05m)
-            //{
-            //    resultado = calculo_IMP * ConstantIRRF.IRRF015;
-            //}
-            //else
-            //if (calculo_IMP >= 3751.05m && calculo_IMP <= 4664.68m)
-            //{
-            //    resultado = calculo_IMP * ConstantIRRF.IRRF0275;
-            //}
-            //return resultado;
-
             var salario = new Salario();
-            var calculo_IMP = salario.CalcularDescontoDependentes(renda, dependentes);
-            return CalcularSalarioIRRF(calculo_IMP);
+            var rendaDependentes = salario.CalcularDescontoDependentes(renda, dependentes);
+            return CalcularSalarioIRRF(rendaDependentes);
         }
 
-        private static decimal CalcularSalarioIRRF(decimal calculo_IMP)
+        private static decimal CalcularSalarioIRRF(decimal rendaDependentes)
         {
-            switch (calculo_IMP)
+            switch (rendaDependentes)
             {
-                case var d when d <= SALARIO190398:
+                case var renda when renda <= ConstantIRRF.RENDA190398:
                     return 0;
-                case var d when d > SALARIO190398 && d <= SALARIO282665:
-                    return calculo_IMP * IRRF0075;
-                case var d when d > SALARIO282665 && d <= SALARIO375105:
-                    return calculo_IMP * IRRF015;
-                case var d when d > SALARIO375105 && d <= SALARIO466468:
-                    return calculo_IMP * IRRF0275;
+                case var renda when renda > ConstantIRRF.RENDA190398 && renda <= ConstantIRRF.RENDA282665:
+                    return rendaDependentes * ConstantIRRF.IRRF0075;
+                case var renda when renda > ConstantIRRF.RENDA282665 && renda <= ConstantIRRF.RENDA375105:
+                    return rendaDependentes * ConstantIRRF.IRRF015;
+                case var renda when renda > ConstantIRRF.RENDA375105 && renda <= ConstantIRRF.RENDA466468:
+                    return rendaDependentes * ConstantIRRF.IRRF0275;
                 default:
                     return 0;
             }
-            return formulaIRRF(calculo_IMP, calculo_IMP <= 1903.98m ? 0 : 
-                                            calculo_IMP <= 2826.65m ? ConstantIRRF.IRRF0075: 
-                                            calculo_IMP <= 3751.05m ? ConstantIRRF.IRRF015 :
-                                            calculo_IMP <= 4664.68m ? ConstantIRRF.IRRF0275 :
-                                            0);
-        }
-
-        private decimal formulaIRRF(decimal calcImp, decimal IRRF)
-        {
-            return calcImp * IRRF;
         }
     }
 }
--- INSS/INSS/Util/ConstantIRRF.cs
+++ INSS/INSS/Util/ConstantIRRF.cs
@@ -11,5 +11,9 @@ namespace INSS.Util
         public const decimal IRRF0075 = 0.075m;
         public const decimal IRRF015 = 0.15m;
         public const decimal IRRF0275 = 0.275m;
+        public const decimal RENDA190398 = 1903.98m;
+        public const decimal RENDA282665 = 2826.65m;
+        public const decimal RENDA375105 = 3751.05m;
+        public const decimal RENDA466468 = 4664.68m;
     }
 }
