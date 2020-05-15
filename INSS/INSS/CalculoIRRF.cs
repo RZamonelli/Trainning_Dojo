95e10bc5afe42249913de158c21500a0ac498b2f
rodolfo <rodolfo.hernandez@philips.com>
2020-05-15 13:50:08 -0300

    Refatorando IRRF.

--- INSS/INSS/CalculoIRRF.cs
+++ INSS/INSS/CalculoIRRF.cs
@@ -11,30 +11,43 @@ namespace INSS
     {
         public decimal CalcularIRRF(decimal renda, int dependentes)
         {
+            //var salario = new Salario();
+            //var calculo_IMP = salario.CalcularDescontoDependentes(renda, dependentes);
+            //decimal resultado = 0;
+
+            //if (calculo_IMP <= 1903.98m)
+            //{
+            //    resultado = 0;
+            //}
+            //else
+            //if (calculo_IMP >= 1903.08m && calculo_IMP <= 2826.65m)
+            //{
+            //    resultado = calculo_IMP * ConstantIRRF.IRRF0075;
+            //}
+            //else
+            //if (calculo_IMP >= 2826.65m && calculo_IMP <= 3751.05m)
+            //{
+            //    resultado = calculo_IMP * ConstantIRRF.IRRF015;
+            //}
+            //else
+            //if (calculo_IMP >= 3751.05m && calculo_IMP <= 4664.68m)
+            //{
+            //    resultado = calculo_IMP * ConstantIRRF.IRRF0275;
+            //}
+            //return resultado;
+
             var salario = new Salario();
             var calculo_IMP = salario.CalcularDescontoDependentes(renda, dependentes);
-            decimal resultado = 0;
+            return formulaIRRF(calculo_IMP, calculo_IMP <= 1903.98m ? 0 : 
+                                            calculo_IMP <= 2826.65m ? ConstantIRRF.IRRF0075: 
+                                            calculo_IMP <= 3751.05m ? ConstantIRRF.IRRF015 :
+                                            calculo_IMP <= 4664.68m ? ConstantIRRF.IRRF0275 :
+                                            0);
+        }
 
-            if (calculo_IMP <= 1903.98m)
-            {
-                resultado = 0;
-            }
-            else
-            if (calculo_IMP >= 1903.08m && calculo_IMP <= 2826.65m)
-            {
-                resultado = calculo_IMP * ConstantIRRF.IRRF0075;
-            }
-            else
-            if (calculo_IMP >= 2826.65m && calculo_IMP <= 3751.05m)
-            {
-                resultado = calculo_IMP * ConstantIRRF.IRRF015;
-            }
-            else
-            if (calculo_IMP >= 3751.05m && calculo_IMP <= 4664.68m)
-            {
-                resultado = calculo_IMP * ConstantIRRF.IRRF0275;
-            }
-            return resultado;
+        private decimal formulaIRRF(decimal calcImp, decimal IRRF)
+        {
+            return calcImp * IRRF;
         }
     }
 }
