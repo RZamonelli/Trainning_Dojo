4d7afc7f002afb543a09c55c13239d3d950cffad
rodolfo <rodolfo.hernandez@philips.com>
2020-06-01 17:49:07 -0300

    Add Calculo Dependentes

--- INSS/INSSUnit/Calculos.cs
+++ INSS/INSSUnit/Calculos.cs
@@ -83,8 +83,9 @@ namespace INSSUnit
         [TestMethod]
         public void TestDependents2000()
         {
-            var dependents = new Salario();
-            decimal resultado = dependents.CalcularDescontoDependentes(2000m, 2);
+            var dependents = new Dependentes();
+            dependents.LerDependentes(2);
+            decimal resultado = dependents.CalcularDescontoDependentes(2000m);
 
             Assert.AreEqual(1620.82m, resultado);
         }
