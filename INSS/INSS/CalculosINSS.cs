9e7f37218b365428318c99beeaf56351642b80f9
rodolfo <rodolfo.hernandez@philips.com>
2020-06-01 17:27:44 -0300

    Codigo faltante

--- INSS/INSS/CalculosINSS.cs
+++ INSS/INSS/CalculosINSS.cs
@@ -10,34 +10,13 @@ namespace INSS
    
     public class CalculosINSS
     {
-        //bool IsInss008v 
         public decimal CalculoINSS(decimal renda)
         {
-            //decimal calculo_INSS = 0;
-            //if (renda <= 1399.12m)
-            //{
-            //    calculo_INSS = renda * INSS008;
-            //}
-            //if (renda >= 1399.13m && renda <= 2331.88m)
-            //{
-            //    calculo_INSS = renda * INSS009;
-            //}
-            //else
-            //{
-            //    calculo_INSS = renda * INSS011;
-            //}
+            return formulaINSS(renda, renda <= 1903.98m ? ConstantINSS.INSS008 :
+                                      renda <= 2331.88m ? ConstantINSS.INSS009 :
+                                      renda <= 3751.05m ? ConstantIRRF.IRRF015 :
+                                      ConstantINSS.INSS011);
 
-            switch (renda)
-            {
-                case var expression when renda <= 1399.12m:
-                    return formulaINSS(renda, ConstantINSS.INSS008);
-                case var expression when (renda >= 1399.13m && renda <= 2331.88m):
-                    return formulaINSS(renda, ConstantINSS.INSS009);
-                default:
-                    return formulaINSS(renda, ConstantINSS.INSS011);
-            }
-
-           
         }
 
         private decimal formulaINSS(decimal renda, decimal Inss){
